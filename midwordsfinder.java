56b8c93fc11c0ec76e5336f7adb77e35864c896c
Ishahriyar <ishahriyar@users.noreply.github.com>
2014-02-01 22:41:00 +0600

    Create midwordsfinder.java

---
+++ midwordsfinder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class print {
+
+	public static void main(String[] args) {
+	
+	System.out.print("Dude Enter your name:");
+	String mystr= new String();
+	Scanner sc=new Scanner(System.in);
+	mystr=sc.nextLine();
+	int a=mystr.length();
+	int b=a/2;
+	if(a%2==1){
+	System.out.println("Middle letter in your name:"+mystr.charAt(b));
+	}
+	else{
+	int d=b-1;
+		
+	System.out.print("Middle letters in your name:"+mystr.charAt(d)+mystr.charAt(b));
+	}
+	}
+}
